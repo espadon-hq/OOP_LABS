1a10f18695eb15b736062ee385dfd384749ea2fe
espadon-hq <vitashi02@gmail.com>
2025-02-22 18:26:38 +0200

    lab 2 done

---
+++ lab2/src/Student.java
@@ -0,0 +1,95 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class Student {
+    private int id;
+    private String lastName;
+    private String firstName;
+    private String middleName;
+    private LocalDate birthDate;
+    private String phoneNumber;
+    private String faculty;
+    private int course;
+    private String group;
+    public Student(int id, String lastName, String firstName, String middleName, LocalDate birthDate, String
+            phoneNumber, String faculty, int course, String group) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.birthDate = birthDate;
+        this.phoneNumber = phoneNumber;
+        this.faculty = faculty;
+        this.course = course;
+        this.group = group;
+    }
+    // Геттери та сеттери для всіх атрибутів
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getMiddleName() {
+        return middleName;
+    }
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getFaculty() {
+        return faculty;
+    }
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+    public int getCourse() {
+        return course;
+    }
+    public void setCourse(int course) {
+        this.course = course;
+    }
+    public String getGroup() {
+        return group;
+    }
+    public void setGroup(String group) {
+        this.group = group;
+    }
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return "Student{" +
+                "id=" + id +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", birthDate=" + birthDate.format(formatter) +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", faculty='" + faculty + '\'' +
+                ", course=" + course +
+                ", group='" + group + '\'' +
+                '}';
+    }
+}
