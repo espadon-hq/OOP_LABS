67107a6e22c779e289f96f27a9ee82288e4417e4
espadon-hq <vitashi02@gmail.com>
2025-02-27 19:36:20 +0200

    added methods calculateR, calculateS, inputInitialValues

--- lab1/lab1/src/Main.java
+++ lab1/lab1/src/Main.java
@@ -6,15 +6,32 @@ public class Main {
     private double b;
     private double x;
 
-    public Main(double a, double b, double x) {
-        this.a = a;
-        this.b = b;
-        this.x = x;
+    public Main() {}
+
+    public void inputInitialValues() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть значення a: ");
+        this.a = scanner.nextDouble();
+
+        System.out.print("Введіть значення b: ");
+        this.b = scanner.nextDouble();
+
+        System.out.print("Введіть значення x: ");
+        this.x = scanner.nextDouble();
     }
 
-    public void calculateAndPrintResults() {
-        double R = (Math.pow(x, 2) * (x + 1)) / b - Math.pow(Math.sin(x + a), 2);
-        double S = Math.sqrt((x * b) / a) + Math.pow(Math.cos(x + Math.pow(b, 3)), 2);
+    public double calculateR() {
+        return (Math.pow(x, 2) * (x + 1)) / b - Math.pow(Math.sin(x + a), 2);
+    }
+
+    public double calculateS() {
+        return Math.sqrt((x * b) / a) + Math.pow(Math.cos(x + Math.pow(b, 3)), 2);
+    }
+
+    public void printResults() {
+        double R = calculateR();
+        double S = calculateS();
         System.out.printf("R = %.4f\n", R);
         System.out.printf("S = %.4f\n", S);
     }
@@ -28,23 +45,13 @@ public class Main {
     }
 
     public void run() {
-        calculateAndPrintResults();
+        inputInitialValues();
+        printResults();
         printDateTime();
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введіть значення a: ");
-        double a = scanner.nextDouble();
-
-        System.out.print("Введіть значення b: ");
-        double b = scanner.nextDouble();
-
-        System.out.print("Введіть значення x: ");
-        double x = scanner.nextDouble();
-
-        Main program = new Main(a, b, x);
+        Main program = new Main();
         program.run();
     }
 }
